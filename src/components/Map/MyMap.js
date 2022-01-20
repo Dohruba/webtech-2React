30f7f9a447679f8e224b5718dbd324325ac86a7f
Dohruba <hendrichsricardo@gmail.com>
2022-01-20 11:45:06 +0100

    Country filtering works.

--- src/components/Map/MyMap.js
+++ src/components/Map/MyMap.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import mapData from "../../data/mapData.json";
-import './MyMap.css';
+import "./MyMap.css";
 
 const MyMap = () => {
+  //const BASE_URL = "https://travelsitebackend.herokuapp.com";
+  const BASE_URL = "http://localhost:5000";
   let state = {};
-  let countryStyle ={
+  let countryStyle = {
     fillColor: "black",
     fillOpacity: 0.75,
-    color: "#9d8c2f"
-  }
+    color: "#a9801a",
+    weight: 1.5,
+  };
+  let visitedCountryStyle = {
+    fillColor: "#860bb8",
+    fillOpacity: 0.5,
+    color: "#a9801a",
+    weight: 1.5,
+  };
+  const [visitedGeo, setVisitedGeo] = useState("");
+  const [nonVisitedGeo, setNonVisitedGeo] = useState("");
 
+  let trips = ["Germany", "Italy"];
+  const filteredData = {
+    ...mapData,
+    features: mapData.features.filter((feature) =>
+      trips.includes(feature.properties.name)
+    ),
+  };
+  const filteredNotVisited = {
+    ...mapData,
+    features: mapData.features.filter(
+      (feature) => !trips.includes(feature.properties.name)
+    ),
+  };
+
+  const visitedCountries = (country, layer) => {
+    console.log(country);
+  };
 
   return (
     <MapContainer
@@ -24,7 +52,15 @@ const MyMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <GeoJSON style={countryStyle} data={mapData.features}></GeoJSON>
+      <GeoJSON
+        style={visitedCountryStyle}
+        data={filteredData.features}
+        onEachFeature={visitedCountries}
+      ></GeoJSON>
+      <GeoJSON
+        style={countryStyle}
+        data={filteredNotVisited.features}
+      ></GeoJSON>
     </MapContainer>
   );
 };
