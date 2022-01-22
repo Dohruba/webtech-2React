d710f3a556860dbfa085b3d997e84b6d4bcba7fd
Dohruba <hendrichsricardo@gmail.com>
2022-01-22 16:21:27 +0100

    Translation for Login and Header

--- src/components/i18n/i18n.js
+++ src/components/i18n/i18n.js
@@ -21,14 +21,41 @@ i18n
         translation: {
           description:{
             tripname: 'Tripname: '
-          }
-          // here we will place our translations...
+          },
+          loginForm:{
+            password: "Password",
+            button: "Login",
+            welcome1: "Login and manage your trips",
+            welcome2: "simply with your own travel map."
+          },
+          header:{
+            title: "Your travelmap to Nirvana",
+            button1: "Map",
+            button2: "Add trip",
+            button3: "Edit trip",
+            button4: "Logout"
+          },
+
         }
       },
       de: {
         translation: {
           description:{
             tripname: 'Reisename: '
+          }
+          ,
+          loginForm:{
+            password: "Passwort",
+            button: "Einloggen",
+            welcome1: "Jetzt einloggen und deine Reisen",
+            welcome2: "ganz einfach über deine individuelle Reisekarte verwalten."
+          },
+          header:{
+            title: "Deine Reisekarte ins Nirwana",
+            button1: "Karte",
+            button2: "Reise hinzufügen",
+            button3: "Reise bearbeiten",
+            button4: "Logout"
           }
           // here we will place our translations...
         }
